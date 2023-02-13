fbd65e8a05b09bf109b2fa5a731c4f820207432a
Shayne <asmbuac@gmail.com>
2023-02-12 19:55:07 -0800

    Added if-else statements to check if cards match

--- scripts.js
+++ scripts.js
@@ -1,7 +1,33 @@
 const cards = document.querySelectorAll('.memory-card')
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     this.classList.toggle('flip')
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // check if cards match
+        if (firstCard.dataset.group === secondCard.dataset.group) {
+            // is a match
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            // not a match
+            setTimeout (() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1500);
+        }
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
