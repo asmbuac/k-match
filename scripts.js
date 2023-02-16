99bbb57d705d82794adb22806f54cafb4732164e
Shayne <asmbuac@gmail.com>
2023-02-15 23:16:25 -0800

    Added functional pause and play buttons

--- scripts.js
+++ scripts.js
@@ -3,8 +3,11 @@ const timeValue = document.getElementById('time');
 const startButton = document.getElementById('start');
 const playAgainButton = document.getElementById('play-again');
 const restartButton = document.getElementById('restart');
+const pauseButton = document.getElementById('pause')
+const playButton = document.getElementById('play')
 const gameContainer = document.querySelector('.memory-game');
 const result = document.getElementById('result');
+const instructions = document.getElementById('instructions')
 const controls = document.querySelector('.controls-container')
 let cards;
 let interval;
@@ -69,6 +72,29 @@ const generateRandom = (size = 4) => {
     return cardValues;
 };
 
+// Generate game board matrix
+const matrixGenerator = (cardValues, size = 4) => {
+    gameContainer.innerHTML = '';
+    cardValues = [...cardValues, ...cardValues];
+    //simple shuffle
+    cardValues.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < size * size; i++) {
+        // Create cards
+        gameContainer.innerHTML += `
+        <div class="memory-card" data-group="${cardValues[i].name}">
+            <img src="${cardValues[i].image}" class="front-face">
+            <img src="img/backface.png" class="back-face">
+        </div>
+        `;
+    }
+    //Grid
+    gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
+
+    // Cards
+    cards = document.querySelectorAll('.memory-card');
+    cards.forEach(card => card.addEventListener('click', flipCard));
+}
+
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
@@ -126,40 +152,12 @@ function checkWin() {
     winCount += 1;
     // check if player won
     if (winCount == Math.floor(8)) {
+        let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+        let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
         result.innerHTML = `<h2>You won!</h2>
-        <h4>Moves: ${movesCount}</h4>`;
+        <h4>Moves: ${movesCount} | Time: ${minutesValue}:${secondsValue}</h4>`;
         stopGame();
     }
-}
-
-// Stop game
-function stopGame() {
-    controls.classList.remove('hide');
-    playAgainButton.classList.remove('hide');
-    clearInterval(interval);
-}
-
-// Generate game board matrix
-const matrixGenerator = (cardValues, size = 4) => {
-    gameContainer.innerHTML = '';
-    cardValues = [...cardValues, ...cardValues];
-    //simple shuffle
-    cardValues.sort(() => Math.random() - 0.5);
-    for (let i = 0; i < size * size; i++) {
-        // Create cards
-        gameContainer.innerHTML += `
-        <div class="memory-card" data-group="${cardValues[i].name}">
-            <img src="${cardValues[i].image}" class="front-face">
-            <img src="img/backface.png" class="back-face">
-        </div>
-        `;
-    }
-    //Grid
-    gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
-
-    // Cards
-    cards = document.querySelectorAll('.memory-card');
-    cards.forEach(card => card.addEventListener('click', flipCard));
 }
 
 // Start game
@@ -167,9 +165,12 @@ startButton.addEventListener('click', () => {
     movesCount = 0;
     seconds = 0;
     minutes = 0;
+    // Hide instructions
+    instructions.classList.add('hide');
     // controls and buttons visibility
     controls.classList.add('hide');
     restartButton.classList.remove('hide');
+    pauseButton.classList.remove('hide');
     startButton.classList.add('hide');
     // Start time
     interval = setInterval(timeGenerator, 1000);
@@ -178,13 +179,23 @@ startButton.addEventListener('click', () => {
     initializer();
 });
 
+// Stop game
+function stopGame() {
+    controls.classList.remove('hide');
+    playAgainButton.classList.remove('hide');
+    gameContainer.classList.add('hide');
+    clearInterval(interval);
+}
+
 // Play again
 playAgainButton.addEventListener( 'click', () => {
     movesCount = 0;
     seconds = 0;
     minutes = 0;
+    gameContainer.classList.remove('hide');
     controls.classList.add('hide');
     restartButton.classList.remove('hide');
+    pauseButton.classList.remove('hide');
     playAgainButton.classList.add('hide');
     interval = setInterval(timeGenerator, 1000);
     moves.innerHTML = `<span>Moves: </span>${movesCount}`;
@@ -200,6 +211,22 @@ restartButton.addEventListener('click', () => {
     moves.innerHTML = `<span>Moves: </span>${movesCount}`;
     initializer();
 });
+
+// Pause game
+pauseButton.addEventListener('click', () => {
+    clearInterval(interval);
+    lockBoard = true;
+    pauseButton.classList.add('hide');
+    playButton.classList.remove('hide');
+})
+
+// Play after pause
+playButton.addEventListener('click', () => {
+    interval = setInterval(timeGenerator, 1000);
+    lockBoard = false;
+    playButton.classList.add('hide');
+    pauseButton.classList.remove('hide');
+})
 
 // Initialize values and function calls
 const initializer = () => {
